bfa70cb6580d1abd1c13263991952278997cf9f1
Турмахан Динислам Даулетулы <@corp.itstep>
2019-06-03 17:21:04 +0600

    release

---
+++ MoneySyncApp/MoneyOperation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace MoneySyncApp
+{
+
+    public class Bank
+    {
+        private object lockObject = new object();
+        private double _money = 1000;
+        private int _random;
+
+        public void MoneyOperationWithSync()
+        {
+            lock (lockObject)
+            {
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    _random = new Random().Next(100);
+                    if (_random % 2 == 0)
+                    {
+                        Thread.Sleep(5 * new Random().Next(100));
+                        _money = _money + 150;
+                        Console.Write("Операция #" + i + " " + "На счету " + _money + "\n");
+                    }
+                    else
+                    {
+                        Thread.Sleep(5 * new Random().Next(100));
+                        _money = _money - 150;
+                        Console.Write("Операция #" + i + " " + "На счету " + _money + "\n");
+                    }
+                }
+            }
+        }
+        public void MoneyOperationWithoutSync()
+        {     
+            for (int i = 1; i <= 10; i++)
+            {
+                _random = new Random().Next(100);
+                if (_random % 2 == 0)
+                {
+                    Thread.Sleep(5 * new Random().Next(100));
+                    _money = _money + 150;
+                    Console.Write("Операция #" + i + " " + "На счету " + _money + "\n");
+                }
+                else
+                {
+                    Thread.Sleep(5 * new Random().Next(100));
+                    _money = _money - 150;
+                    Console.Write("Операция #" + i + " " + "На счету " + _money + "\n");
+                }
+            }
+
+        }
+    }
+}
---
+++ MoneySyncApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MoneySyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            var threads = new  Thread[10];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("------------Синхронизация--------------");
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(bank.MoneyOperationWithSync);
+                thread.Name = $"Поток номер {thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("------------Без синхронизации-------------");
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(bank.MoneyOperationWithoutSync);
+                thread.Name = $"Поток номер {thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
